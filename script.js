625ee425a9a51934e5d4a6cb5a3722409d6b0d25
gabrielebarcellona <gabrerozabarcy5@gmail.com>
2023-05-17 09:50:07 +0200

    add ciclo for

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const app = Vue.createApp({
     data(){
         return{
             EmailList : [
-
+                
             ]
+            
         }
     },
     methods:{
         getRandomEmail(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((result) => this.EmailList.push(result.data.response))
-        }
+            for(i=0 ; i < 10 ; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((result) => this.EmailList.push(result.data.response))
+            }
+          
+    },
     },
 
     created(){
